a818318deffa6c5032786c54c57978ec51458b0c
Nella Rory <nellaror@ji-f4.tash.21-school.uz>
2025-04-01 14:25:46 +0500

    Add set_sign func

--- code/common/common.h
+++ code/common/common.h
@@ -6,7 +6,7 @@
 void s21_zero(s21_decimal *value);
 
 int s21_get_sign(s21_decimal value);
-// void s21_set_sign(s21_decimal *value);
+void s21_set_sign(s21_decimal *value, int sign);
 
 int s21_get_scale(s21_decimal value);
 void s21_set_scale(s21_decimal *value, int scale);
