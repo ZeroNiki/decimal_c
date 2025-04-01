61c45b48d526140bb97839a0a6b4e3cf6ceb4187
Nella Rory <nellaror@ji-f4.tash.21-school.uz>
2025-04-01 15:01:22 +0500

    Add s21_negate func

--- code/common/common.h
+++ code/common/common.h
@@ -11,8 +11,8 @@ void s21_set_sign(s21_decimal *value, int sign);
 int s21_get_scale(s21_decimal value);
 void s21_set_scale(s21_decimal *value, int scale);
 
+void s21_negate(s21_decimal *value);
 // void s21_normalize(s21_decimal value1, s21_decimal value2);
-// void s21_negate(s21_decimal value, s21_decimal *result);
 
 // int s21_get_bit(s21_decimal value, int index);
 
